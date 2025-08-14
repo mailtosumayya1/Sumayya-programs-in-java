26013aefecec305c4200793e2e70f49214c529df
mailtosumayya1 <mailtosumayya1@gmail.com>
2025-08-14 10:43:59 +0530

    Add files via upload

---
+++ NeonNumber.java
@@ -0,0 +1,17 @@
+class NeonNumber{
+    public static void main{
+        int num=9;
+        int sq=num*num;
+        int sum=0;
+        while(sq>0){
+            sum+=sq%10;
+            sq/=10;
+        }
+        if(sum==num){
+            System.out.println(num+"is a Neon Number");
+            else{
+                System.out.println(num+"is Not a Neon Number");
+            }
+        }
+    }
+}
